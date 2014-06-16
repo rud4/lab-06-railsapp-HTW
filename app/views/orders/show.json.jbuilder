5225d20c1be5fe80df6d0435c394c983b2c39771
B Kleinen <kleinen@infrastructure.de>
2014-06-16 10:16:47 +0200

    rails g scaffold Order customer_id:integer placed_on:date status:string

---
+++ app/views/orders/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @order, :id, :customer_id, :placed_on, :status, :created_at, :updated_at
---
+++ db/migrate/20140616081634_create_orders.rb
@@ -0,0 +1,11 @@
+class CreateOrders < ActiveRecord::Migration
+  def change
+    create_table :orders do |t|
+      t.integer :customer_id
+      t.date :placed_on
+      t.string :status
+
+      t.timestamps
+    end
+  end
+end
