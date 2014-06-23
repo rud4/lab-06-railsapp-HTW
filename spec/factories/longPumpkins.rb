e68fde60b4fe4fcc7d7bf00929e0a5e412e297a7
duclong793 <duclong793@live.de>
2014-06-23 13:31:42 +0200

    new factory file

---
+++ spec/factories/longPumpkins.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :carnival class: Pumpkin do
+    name 'Carnival'
+    color 'orange, black, white'
+    size  's'
+    price '3.5'
+  end
+  factory :kabobcha, class: Pumpkin do
+    name 'Kabobcha'
+    color 'black'
+    size  's'
+    price '3.0'
+  end
+end
